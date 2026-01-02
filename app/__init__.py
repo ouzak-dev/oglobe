d3e1cc377cfeee303b0e9d9ac80dfa9def3fd1ac
ZAKARIA AIT OUCHATTOU <69395654+ouzak-dev@users.noreply.github.com>
2026-01-02 14:43:37 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,14 +11,15 @@ app = Flask(__name__)
 # For development, a random key is generated (but will change on restart)
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
 # contact mail
-app.config['MAIL_CONTACT'] = "contact@oglobe.ma"
+app.config['MAIL_CONTACT'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
 # Mail credentials
-app.config['MAIL_SERVER'] = "mail.privateemail.com"
-app.config['MAIL_PORT'] = 465
-app.config['MAIL_USERNAME'] = "auto@example.com"
-app.config['MAIL_PASSWORD'] = "Password@example"
+app.config['MAIL_SERVER'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
+app.config['MAIL_PORT'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
+app.config['MAIL_USERNAME'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
+app.config['MAIL_PASSWORD'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 MAIL = Mail(app)
 
 from app import routes
+

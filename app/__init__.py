72a194979de7db58567c2d47c7a96104fcdd3c65
ZAKARIA AIT OUCHATTOU <69395654+ouzak-dev@users.noreply.github.com>
2026-01-02 14:44:14 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -11,15 +11,16 @@ app = Flask(__name__)
 # For development, a random key is generated (but will change on restart)
 app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
 # contact mail
-app.config['MAIL_CONTACT'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
+app.config['MAIL_CONTACT'] = os.environ.get('MAIL_CONTACT')
 # Mail credentials
-app.config['MAIL_SERVER'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
-app.config['MAIL_PORT'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
-app.config['MAIL_USERNAME'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
-app.config['MAIL_PASSWORD'] = os.environ.get('SECRET_KEY') or secrets.token_hex(32)
+app.config['MAIL_SERVER'] = os.environ.get('MAIL_SERVER')
+app.config['MAIL_PORT'] = os.environ.get('MAIL_PORT')
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
 app.config['MAIL_USE_TLS'] = False
 app.config['MAIL_USE_SSL'] = True
 MAIL = Mail(app)
 
 from app import routes
+
 
